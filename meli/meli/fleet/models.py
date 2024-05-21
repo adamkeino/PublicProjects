67ff2e967853aeb83b11352fbb8beaac216b66e6
Adam Keino <keinoadamdev@gmail.com>
2024-05-21 17:09:12 +0300

    Database models created

--- meli/meli/fleet/models.py
+++ meli/meli/fleet/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Vehicle(models.Models):
+    vehicle_type = models.CharField(max_length=200)
+    vehicle_name = models.CharField(max_length=200)
+    vehicle_registration = models.CharField(max_length=200)
+    """
+        vehicle_location = https://django-location-field.readthedocs.io/en/latest/index.html
+        vehicle_speed = 
+
+    """
+    car = f"Model: {vehicle_type} \n Name: {vehicle_name} \n Reg. No: {vehicle_registration}"
+    def __str__(self):
+        return self.car
+
+class Driver(models.Models):
+    driver_name = models.CharField(max_length=100)
+    driver_id = models.IntegerField(length=50)
+
+
+
+
